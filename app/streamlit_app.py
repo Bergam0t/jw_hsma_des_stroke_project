3d054fa958385588c79c5925be847a4c41371b47
amyheather <a.heather2@exeter.ac.uk>
2026-01-21 13:35:25 +0000

    style(app streamlit_app): module docstring, PEP line length

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,7 +1,15 @@
+"""
+Set-up the app (called as `streamlit run app/streamlit_app.py`).
+"""
+
 import streamlit as st
 
+
 pg = st.navigation(
-    [st.Page("run_model.py", title="Run the Model"), st.Page("about.py", title="About")],
+    [
+        st.Page("run_model.py", title="Run the Model"),
+        st.Page("about.py", title="About")
+    ],
     position="top",
 )
 pg.run()
