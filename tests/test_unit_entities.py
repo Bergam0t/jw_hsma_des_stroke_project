3d0bf2ae14b36b1843e7949bc7b706325da64e3a
amyheather <a.heather2@exeter.ac.uk>
2026-01-23 11:06:23 +0000

    refactor(test_entities): improve design of the test

--- tests/test_unit_entities.py
+++ tests/test_unit_entities.py
@@ -9,115 +9,107 @@ from stroke_ward_model.entities import Patient
 
 
 @pytest.mark.parametrize(
-    "attr, expected_type, allowed_values, is_nan, default_value",
+    "attr, expected_type, default_kind, default_value, allowed_values",
     [
         # Identifiers
-        ("id", (int, str), None, False, 123),
+        ("id", (int, str), "value", 123, None),
 
         # Queue times
-        ("q_time_nurse", (float, np.floating), None, True, None),
-        ("q_time_ward", (float, np.floating), None, True, None),
+        ("q_time_nurse", (float, np.floating), "nan", None, None),
+        ("q_time_ward", (float, np.floating), "nan", None, None),
 
         # Onset, MRS, diagnosis
-        ("onset_type",
-         (int, float, np.floating), {0, 1, 2, np.NaN}, True, None),
-        ("mrs_type", (int, float, np.floating), None, True, None),
-        ("mrs_discharge", (int, float, np.floating), None, True, None),
-        ("diagnosis", (int, float, np.floating), None, True, None),
+        ("onset_type", (float, np.floating), "nan", None, {0, 1, 2}),
+        ("mrs_type", (float, np.floating), "nan", None, None),
+        ("mrs_discharge", (float, np.floating), "nan", None, None),
+        ("diagnosis", (float, np.floating), "nan", None, None),
         ("patient_diagnosis",
-         (int, float, np.floating), {0, 1, 2, 3, 4, np.NaN}, True, None),
-        ("patient_diagnosis_type", (str,), {"None", None}, False, "None"),
+         (float, np.floating), "nan", None, {0, 1, 2, 3, 4}),
+        ("patient_diagnosis_type", (str,), "value", "None", {"None"}),
 
         # Priority and admission likelihood
-        ("priority", (int,), {1}, False, 1),
-        ("non_admission", (int, float, np.floating), None, True, None),
+        ("priority", (int,), "value", 1, {1}),
+        ("non_admission", (float, np.floating), "nan", None, None),
 
         # Pathway flags
-        ("advanced_ct_pathway",
-         (bool, type(None)), {True, False, None}, False, None),
-        ("sdec_pathway", (bool, type(None)), {True, False, None}, False, None),
-        ("thrombolysis", (bool, type(None)), {True, False, None}, False, None),
-        ("thrombectomy", (bool, type(None)), {True, False, None}, False, None),
-        ("admission_avoidance",
-         (bool, type(None)), {True, False, None}, False, None),
-        ("non_admitted_tia_ns_sm",
-         (bool, type(None)), {True, False, None}, False, None),
+        ("advanced_ct_pathway", (bool,), "none", None, None),
+        ("sdec_pathway", (bool,), "none", None, None),
+        ("thrombolysis", (bool,), "none", None, None),
+        ("thrombectomy", (bool,), "none", None, None),
+        ("admission_avoidance", (bool,), "none", None, None),
+        ("non_admitted_tia_ns_sm", (bool,), "none", None, None),
 
         # Lengths of stay and durations
-        ("ward_los", (float, np.floating), None, True, None),
-        ("ward_los_thrombolysis", (float, np.floating), None, True, None),
-        ("sdec_los", (float, np.floating), None, True, None),
-        ("ctp_duration", (float, np.floating), None, True, None),
-        ("ct_duration", (float, np.floating), None, True, None),
+        ("ward_los", (float, np.floating), "nan", None, None),
+        ("ward_los_thrombolysis", (float, np.floating), "nan", None, None),
+        ("sdec_los", (float, np.floating), "nan", None, None),
+        ("ctp_duration", (float, np.floating), "nan", None, None),
+        ("ct_duration", (float, np.floating), "nan", None, None),
 
         # Arrival timing
-        ("arrived_ooh", (bool, type(None)), {True, False, None}, False, None),
+        ("arrived_ooh", (bool,), "none", None, None),
 
         # Clock / event times
-        ("clock_start", (float, np.floating), None, True, None),
-        ("nurse_q_start_time", (float, np.floating), None, True, None),
-        ("nurse_triage_start_time", (float, np.floating), None, True, None),
-        ("nurse_triage_end_time", (float, np.floating), None, True, None),
+        ("clock_start", (float, np.floating), "nan", None, None),
+        ("nurse_q_start_time", (float, np.floating), "nan", None, None),
+        ("nurse_triage_start_time", (float, np.floating), "nan", None, None),
+        ("nurse_triage_end_time", (float, np.floating), "nan", None, None),
 
         # CT / CTP timing
-        ("ct_scan_start_time", (float, np.floating), None, True, None),
-        ("ct_scan_end_time", (float, np.floating), None, True, None),
-        ("ctp_scan_start_time", (float, np.floating), None, True, None),
-        ("ctp_scan_end_time", (float, np.floating), None, True, None),
+        ("ct_scan_start_time", (float, np.floating), "nan", None, None),
+        ("ct_scan_end_time", (float, np.floating), "nan", None, None),
+        ("ctp_scan_start_time", (float, np.floating), "nan", None, None),
+        ("ctp_scan_end_time", (float, np.floating), "nan", None, None),
 
         # SDEC timings
-        ("sdec_admit_time", (float, np.floating), None, True, None),
-        ("sdec_discharge_time", (float, np.floating), None, True, None),
+        ("sdec_admit_time", (float, np.floating), "nan", None, None),
+        ("sdec_discharge_time", (float, np.floating), "nan", None, None),
 
         # Ward timings
-        ("ward_q_start_time", (float, np.floating), None, True, None),
-        ("ward_admit_time", (float, np.floating), None, True, None),
-        ("ward_discharge_time", (float, np.floating), None, True, None),
-        ("exit_time", (float, np.floating), None, True, None),
+        ("ward_q_start_time", (float, np.floating), "nan", None, None),
+        ("ward_admit_time", (float, np.floating), "nan", None, None),
+        ("ward_discharge_time", (float, np.floating), "nan", None, None),
+        ("exit_time", (float, np.floating), "nan", None, None),
 
         # Resource IDs
-        ("nurse_attending_id", (int, float, np.floating), None, True, None),
-        ("ct_scanner_id", (int, float, np.floating), None, True, None),
-        ("sdec_bed_id", (int, float, np.floating), None, True, None),
-        ("ward_bed_id", (int, float, np.floating), None, True, None),
+        ("nurse_attending_id", (float, np.floating), "nan", None, None),
+        ("ct_scanner_id", (float, np.floating), "nan", None, None),
+        ("sdec_bed_id", (float, np.floating), "nan", None, None),
+        ("ward_bed_id", (float, np.floating), "nan", None, None),
 
         # SDEC state flags
-        ("sdec_running_when_required",
-         (bool, type(None)), {True, False, None}, False, None),
-        ("sdec_full_when_required",
-         (bool, type(None)), {True, False, None}, False, None),
+        ("sdec_running_when_required", (bool,), "none", None, None),
+        ("sdec_full_when_required", (bool,), "none", None, None),
 
         # Warm-up / journey flags
-        ("generated_during_warm_up",
-         (bool, type(None)), {True, False, None}, False, None),
-        ("journey_completed",
-         (bool,), {True, False}, False, False),
+        ("generated_during_warm_up", (bool,), "none", None, None),
+        ("journey_completed", (bool,), "value", False, {True, False}),
     ],
 )
 def test_patient_default_attributes(
-    attr, expected_type, allowed_values, is_nan, default_value
+    attr, expected_type, default_kind, default_value, allowed_values
 ):
-    """Check each Patient attribute has the expected type and default value."""
-    # Make a patient and get the attribute
+    """
+    Check each Patient attribute exists and has the expected default semantics.
+    """
     patient = Patient(p_id=123)
     value = getattr(patient, attr)
 
-    # Check the type (also allow None if expected)
-    assert isinstance(value, expected_type) or value is None
-
-    # Check defaults - either should be NaN, or should be the specified default
-    if is_nan:
-        assert isinstance(value, (float, np.floating))
+    if default_kind == "nan":
+        assert isinstance(value, expected_type)
         assert np.isnan(value)
-    else:
-        if default_value is not None:
-            assert value == default_value
 
-    # If we have a list of allowed values, check the value is in it
-    if allowed_values is not None:
-        if isinstance(value, float) and np.isnan(value):
-            # Handle NaN separately: make sure NaN is allowed
-            assert any(isinstance(v, float) and np.isnan(v)
-                       for v in allowed_values)
-        else:
-            assert value in allowed_values
+    elif default_kind == "none":
+        assert value is None
+
+    elif default_kind == "value":
+        assert isinstance(value, expected_type)
+        assert value == default_value
+
+    else:
+        pytest.fail(f"Unknown default_kind '{default_kind}'")
+
+    if allowed_values is not None and value is not None and not (
+        isinstance(value, float) and np.isnan(value)
+    ):
+        assert value in allowed_values
