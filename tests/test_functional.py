a17e81696ffd286f56ee309186c773c19da61dc1
amyheather <a.heather2@exeter.ac.uk>
2026-01-29 14:08:19 +0000

    refactor(test_functional): remove param reset as solved with confest (#30)

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -24,49 +24,6 @@ def run_single_model_with_config(config_overrides=None):
     g.sdec_unav_freq = 60
     g.sdec_unav_time = 10
 
-    # Reset all parameters we override
-    # (As otherwise altered values get carried between tests)
-    g.patient_inter_day = 200.0
-    g.patient_inter_night = 666.666666666667
-    g.number_of_nurses = 2
-    g.number_of_ctp = 1
-    g.sdec_beds = 5
-    g.number_of_ward_beds = 1
-    g.mean_n_consult_time = 60
-    g.mean_n_ct_time = 20
-    g.mean_n_sdec_time = 240
-    g.mean_n_i_ward_time_mrs_0 = 1440 * 2.88
-    g.mean_n_i_ward_time_mrs_1 = 1440 * 4.54
-    g.mean_n_i_ward_time_mrs_2 = 1440 * 7.4
-    g.mean_n_i_ward_time_mrs_3 = 1440 * 14.14
-    g.mean_n_i_ward_time_mrs_4 = 1440 * 26.06
-    g.mean_n_i_ward_time_mrs_5 = 1440 * 29.7
-    g.mean_n_ich_ward_time_mrs_0 = 1440 * 2.62
-    g.mean_n_ich_ward_time_mrs_1 = 1440 * 7.03
-    g.mean_n_ich_ward_time_mrs_2 = 1440 * 12.15
-    g.mean_n_ich_ward_time_mrs_3 = 1440 * 18.91
-    g.mean_n_ich_ward_time_mrs_4 = 1440 * 32.45
-    g.mean_n_ich_ward_time_mrs_5 = 1440 * 41.83
-    g.mean_n_non_stroke_ward_time = 1440 * 3  # 4320
-    g.mean_n_tia_ward_time = 1440 * 1
-    g.thrombolysis_los_save = 0.75
-    g.sdec_dr_cost_min = 0.50
-    g.inpatient_bed_cost = 876
-    g.inpatient_bed_cost_thrombolysis = 528.17
-    g.mean_mrs = 2
-    g.ich = 10
-    g.i = 60
-    g.tia = 70
-    g.stroke_mimic = 80
-    g.tia_admission = 10
-    g.stroke_mimic_admission = 30
-    g.sdec_value = 0
-    g.ctp_value = 0
-    g.sdec_opening_hour = 0
-    g.ctp_opening_hour = 0
-    g.in_hours_start = 7
-    g.ooh_start = 0
-
     # Apply any overrides you want to test
     if config_overrides:
         for name, value in config_overrides.items():
